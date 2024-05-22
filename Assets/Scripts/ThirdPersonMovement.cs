bde55be2456247eb2dac993c0152f8323cefa44c
Mehmet Hayri Çakır <36895137+mhmtckr27@users.noreply.github.com>
2024-05-22 07:08:10 +0300

    NPC nameplate implemented.

--- Assets/Scripts/ThirdPersonMovement.cs
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -1,50 +1,50 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class ThirdPersonMovement : MonoBehaviour
+namespace RPG
 {
-    [SerializeField] private float _rotateSpeed = 1000f;
-    [SerializeField] private float _moveSpeed = 5f;
-    
-    private Rigidbody _rigidbody;
-    private Transform _transform;
-    private Animator _animator;
-    
-    private static readonly int Horizontal = Animator.StringToHash("Horizontal");
-    private static readonly int Vertical = Animator.StringToHash("Vertical");
-
-    private void Awake()
-    { 
-        _rigidbody = GetComponent<Rigidbody>();
-        _transform = GetComponent<Transform>();
-        _animator = GetComponent<Animator>();
-    }
-
-    private void Update()
+    public class ThirdPersonMovement : MonoBehaviour
     {
-        float lookInput = Input.GetAxis("Mouse X");
-        _transform.Rotate(0, lookInput * _rotateSpeed * Time.deltaTime, 0);
-    }
+        [SerializeField] private float _rotateSpeed = 1000f;
+        [SerializeField] private float _moveSpeed = 5f;
+    
+        private Rigidbody _rigidbody;
+        private Transform _transform;
+        private Animator _animator;
+    
+        private static readonly int Horizontal = Animator.StringToHash("Horizontal");
+        private static readonly int Vertical = Animator.StringToHash("Vertical");
 
-    private void FixedUpdate()
-    {
-        float verticalMoveInput = Input.GetAxis("Vertical");
-        float horizontalMoveInput = Input.GetAxis("Horizontal");
+        private void Awake()
+        { 
+            _rigidbody = GetComponent<Rigidbody>();
+            _transform = GetComponent<Transform>();
+            _animator = GetComponent<Animator>();
+        }
 
-        if (Input.GetKey(KeyCode.LeftShift))
-            verticalMoveInput *= 2;
+        private void Update()
+        {
+            float lookInput = Input.GetAxis("Mouse X");
+            _transform.Rotate(0, lookInput * _rotateSpeed * Time.deltaTime, 0);
+        }
 
-        Vector3 velocity = new Vector3(horizontalMoveInput, 0, verticalMoveInput);
-        velocity.Normalize();
-        velocity *= _moveSpeed * Time.fixedDeltaTime;
+        private void FixedUpdate()
+        {
+            float verticalMoveInput = Input.GetAxis("Vertical");
+            float horizontalMoveInput = Input.GetAxis("Horizontal");
 
-        Vector3 moveOffset = _transform.rotation * velocity;
-        Vector3 newPosition = _transform.position + moveOffset;
+            if (Input.GetKey(KeyCode.LeftShift))
+                verticalMoveInput *= 2;
+
+            Vector3 velocity = new Vector3(horizontalMoveInput, 0, verticalMoveInput);
+            velocity.Normalize();
+            velocity *= _moveSpeed * Time.fixedDeltaTime;
+
+            Vector3 moveOffset = _transform.rotation * velocity;
+            Vector3 newPosition = _transform.position + moveOffset;
         
-        _rigidbody.MovePosition(newPosition);
-        _animator.SetFloat(Vertical, verticalMoveInput, 0.1f, Time.deltaTime);
-        _animator.SetFloat(Horizontal, horizontalMoveInput, 0.1f, Time.deltaTime);
+            _rigidbody.MovePosition(newPosition);
+            _animator.SetFloat(Vertical, verticalMoveInput, 0.1f, Time.deltaTime);
+            _animator.SetFloat(Horizontal, horizontalMoveInput, 0.1f, Time.deltaTime);
+        }
     }
 }
---
+++ Assets/Scripts/UI/LookAtCamera.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+namespace RPG.UI
+{
+    public class LookAtCamera : MonoBehaviour
+    {
+        private void LateUpdate()
+        {
+            transform.LookAt(Camera.main.transform);
+            transform.Rotate(0, 180, 0);
+        }
+    }
+}
