2997423a66e11107bd8ee084c551fa713c372703
Mehmet Hayri Çakır <36895137+mhmtckr27@users.noreply.github.com>
2024-05-24 08:01:00 +0300

    Dialog System   |   Imported and implemented dialog system. Created a UI. Made robot NPC a dialog giver.

---
+++ Assets/Scripts/Systems/DialogSystem/DialogGiver.cs
@@ -0,0 +1,21 @@
+﻿using RPG.Systems.DialogSystem.UI;
+using UnityEngine;
+
+namespace RPG.Systems.DialogSystem
+{
+    public class DialogGiver : MonoBehaviour
+    {
+        [SerializeField] private TextAsset dialogAsset;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if(!other.TryGetComponent(out ThirdPersonMovement player))
+                return;
+
+            var dialogController = FindObjectOfType<DialogController>(true);
+            dialogController.gameObject.SetActive(true);
+            dialogController.StartDialog(dialogAsset);
+            transform.LookAt(player.transform);
+        }
+    }
+}
---
+++ Assets/Scripts/Systems/DialogSystem/UI/DialogController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using Ink.Runtime;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace RPG.Systems.DialogSystem.UI
+{
+    public class DialogController : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text dialogText;
+        [SerializeField] private List<Button> responseButtons;
+        
+        private Story _story;
+
+        public void StartDialog(TextAsset dialogAsset)
+        {
+            _story = new Story(dialogAsset.text);
+            RefreshView();
+        }
+
+        private void RefreshView()
+        {
+            var dialogTextBuilder = new StringBuilder();
+
+            while (_story.canContinue)
+            {
+                dialogTextBuilder.Append(_story.Continue());
+            }
+
+            dialogText.SetText(dialogTextBuilder);
+            
+            for (var i = 0; i < responseButtons.Count; i++)
+            {
+                var responseButton = responseButtons[i];
+                var shouldActivate =  i < _story.currentChoices.Count;
+                
+                responseButton.gameObject.SetActive(shouldActivate);
+                responseButton.onClick.RemoveAllListeners();
+                
+                if(!shouldActivate)
+                    continue;
+
+                var choice = _story.currentChoices[i];
+                responseButton.GetComponentInChildren<TMP_Text>().SetText(choice.text);
+                responseButton.onClick.AddListener(() =>
+                {
+                    _story.ChooseChoiceIndex(choice.index);
+                    RefreshView();
+                });
+            }
+        }
+    }
+}
