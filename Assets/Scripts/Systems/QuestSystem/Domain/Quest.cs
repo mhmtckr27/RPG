bd28da2ce28a65daa5479a80e5f98b3c7d843d6a
Mehmet Hayri Çakır <36895137+mhmtckr27@users.noreply.github.com>
2024-05-31 07:02:27 +0300

    Quest System   |   UI Panel code implementation started

--- Assets/Scripts/Systems/QuestSystem/Domain/Quest.cs
+++ Assets/Scripts/Systems/QuestSystem/Domain/Quest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text;
 using UnityEngine;
 
 namespace RPG.Systems.QuestSystem
@@ -7,8 +8,9 @@ namespace RPG.Systems.QuestSystem
     [CreateAssetMenu(menuName = "RPG/Scriptable Objects/Quest System/Quest", fileName = "SO_Quest_")]
     public class Quest : ScriptableObject
     {
-        [field: SerializeField] public string Name { get; private set; }
+        [field: SerializeField] public string DisplayName { get; private set; }
         [field: SerializeField] public string Description { get; private set; }
+        [field: SerializeField] public Sprite Icon { get; private set; }
         [field: SerializeField] public List<Step> Steps { get; private set; }
 
 #if UNITY_EDITOR
@@ -22,12 +24,31 @@ namespace RPG.Systems.QuestSystem
     {
         [field: SerializeField] public string Instructions { get; private set; }
         [field: SerializeField] public List<Objective> Objectives { get; private set; }
+
+        public override string ToString()
+        {
+            var builder = new StringBuilder();
+
+            builder.AppendLine(Instructions);
+            
+            foreach (var objective in Objectives)
+            {
+                builder.AppendLine(objective.ToString());
+            }
+
+            return builder.ToString();
+        }
     }
 
     [Serializable]
     public class Objective
     {
         [field: SerializeField] public ObjectiveType ObjectiveType { get; private set;  }
+
+        public override string ToString()
+        {
+            return ObjectiveType.ToString();
+        }
     }        
     
     public enum ObjectiveType
---
+++ Assets/Scripts/Systems/QuestSystem/Representation/QuestPanelView.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+using System.Text;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace RPG.Systems.QuestSystem.Representation
+{
+    public class QuestPanelView : MonoBehaviour
+    {
+        [SerializeField] private Quest selectedQuest;
+        [SerializeField] private TMP_Text displayNameText;
+        [SerializeField] private TMP_Text descriptionText;
+        [SerializeField] private TMP_Text objectivesText;
+        [SerializeField] private Image icon;
+        
+        private void Awake()
+        {
+            Refresh();
+        }
+
+        private void Refresh()
+        {
+            displayNameText.SetText(selectedQuest.DisplayName);
+            descriptionText.SetText(selectedQuest.Description);
+            icon.sprite = selectedQuest.Icon;
+
+            RefreshObjectivesText();
+        }
+
+        private void RefreshObjectivesText()
+        {
+            var currentStep = selectedQuest.Steps.FirstOrDefault();
+            
+            if(currentStep == null)
+                return;
+            
+            objectivesText.SetText(currentStep.ToString());
+        }
+    }
+}
